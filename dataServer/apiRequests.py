5681337c573eac85fd4d75fb758897f584c080e1
Mark Toda <todamark@cs.washington.edu>
2018-04-21 16:37:33 -0700

    remove print

--- dataServer/apiRequests.py
+++ dataServer/apiRequests.py
@@ -2,5 +2,3 @@ from requests import put, get
 
 def putCheckIn(name, number):
     return put('http://tmobilehack.azurewebsites.net/echo/' + name, data={'data': number}).json()
-
-print(putCheckIn("hi", 5))
