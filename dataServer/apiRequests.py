96b77edc0f71b5c7686218de5a33da7408a2d276
Mark Toda <todamark@cs.washington.edu>
2018-04-21 17:56:03 -0700

    added more requests

--- dataServer/apiRequests.py
+++ dataServer/apiRequests.py
@@ -1,4 +1,15 @@
-from requests import put, get
+from requests import put, get, post
 
 def putCheckIn(name, number):
-    return put('http://tmobilehack.azurewebsites.net/echo/' + name, data={'data': number}).json()
+    return put('http://127.0.0.1:5002/echo', data={'name': name, 'data': number}).json()
+
+def getBestRoom():
+    return get('http://tmobilehack.azurewebsites.net/twilio/workspaces/best').json()
+
+def getWorkspaces():
+    return get('http://tmobilehack.azurewebsites.net/twilio/workspaces/all').json()
+
+def twilioPost():
+    return post('http://tmobilehack.azurewebsites.net/twilio').json()
+
+#print(twilioPost())
